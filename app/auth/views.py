05ee7a5c27c53b8d78a0fe2b39a30748a1dd7f1d
Xukay101 <zaca03zaca@gmail.com>
2023-10-28 15:58:49 -0300

    Add logout system using Redis for token revocation

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,12 +1,14 @@
+from datetime import datetime, timezone
+
 from flask import request, jsonify, Blueprint, current_app
 from marshmallow import ValidationError
 from sqlalchemy.exc import IntegrityError
-from flask_jwt_extended import (jwt_required, create_access_token, current_user)
+from flask_jwt_extended import jwt_required, create_access_token, current_user, get_jwt
 
-from app import db
+from app import db, redis_conn
 from app.schemas import user_schema
 from app.models import User
-from app.auth.handlers import user_identity_lookup, user_lookup_callback
+from app.auth.handlers import *
 
 bp = Blueprint('auth', __name__)
 
@@ -18,12 +20,12 @@ def register():
         user.set_password(user_data['password'])
         db.session.add(user)
         db.session.commit()
-        return jsonify({'message': 'User registered successfully'})
+        return jsonify(message='User registered successfully')
     except ValidationError as err:
         return jsonify(err.messages), 400
     except IntegrityError:
         db.session.rollback()  # Rollback en caso de error
-        return jsonify({'message': 'Username or email already registered'}), 400
+        return jsonify(message='Username or email already registered'), 400
 
 @bp.route('/login', methods=['POST'])
 def login():
@@ -31,7 +33,7 @@ def login():
     password = request.json.get('password')
 
     if not username or not password:
-        return jsonify({'message': 'Username and password are required'}), 400
+        return jsonify(message='Username and password are required'), 400
 
     # Get user in database
     user = User.query.filter_by(username=username).first()
@@ -41,7 +43,19 @@ def login():
         access_token = create_access_token(identity=user)
         return jsonify({'access_token': access_token})
 
-    return jsonify({'message': 'Invalid username or password'}), 401
+    return jsonify(message='Invalid username or password'), 401
+
+@bp.route('/logout', methods=['DELETE'])
+@jwt_required()
+def logout():
+    jti = get_jwt()['jti']
+    token_expires = get_jwt()['exp']
+    now = datetime.now(timezone.utc)
+    remaining_time = token_expires - int(now.timestamp())
+
+    redis_conn.set(jti, '', ex=remaining_time)
+
+    return jsonify(message='Access token revoked')
 
 @bp.route('/verify', methods=['GET'])
 @jwt_required()
--- app/config.py
+++ app/config.py
@@ -1,12 +1,19 @@
 import os
+from datetime import timedelta
 
 class Settings:
     # Configuración general
     APP_NAME = 'MiniBlog'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG = True
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 
-    # Configuración de SQLAlchemy
+    # Configuración SQLAlchemy
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Configuracion JWT Token
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
+
+    # Configuracion Redis
+    REDIS_URL = os.environ.get('REDIS_URL')
